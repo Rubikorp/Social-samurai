cf139d6c8fe44aec2503a4116cd4308e315aaaf3
Ruslan Bikmetov <bik.big102rus@gmail.com>
2022-04-21 22:45:11 +0500

    add project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import Nav from "./components/Navbar";
+import Profile from "./components/Profile";
+
+
+
+function App() {
+  return (
+    <div className="app-wrapper">
+      <Header />
+      <Nav />
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
