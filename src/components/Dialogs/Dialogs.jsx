a9dadac46fe8dc309d60c8f51e0806838f2ba21b
Ruslan Bikmetov <bik.big102rus@gmail.com>
2022-04-23 17:59:09 +0500

    Добавил стилей в Dialogs.jsx

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,16 +1,41 @@
 import React from "react";
 import s from "./Dialogs.module.css"
 
-const Dialogs = (props) => {
+const Dialogs = () => {
     return (
-        <div className='container bg'>
+        <div className='container bg height__8vh'>
+            <h2 className={s.header}>Dialogs</h2>
             <div className={s.containe}>
                 <div className={s.dialogs}>
                     <div className={s.users}>
-                        users
+                        <div className={s.list}>
+                            <div className={s.item + ' ' + s.active}>
+                                Ruslan
+                            </div>
+                            <div className={s.item}>
+                                Roman
+                            </div>
+                            <div className={s.item}>
+                                Rinat
+                            </div>
+                            <div className={s.item}>
+                                Elina
+                            </div>
+                            <div className={s.item}>
+                                Victor
+                            </div>
+                        </div>
                     </div>
-                    <div className={s.chat}>
-                        chat
+                    <div className={s.chats}>
+                        <div className={s.message}>
+                            Hi
+                        </div>
+                        <div className={s.message}>
+                            How are you?
+                        </div>
+                        <div className={s.message}>
+                            Ruslan
+                        </div>
                     </div>
                 </div>
             </div>
