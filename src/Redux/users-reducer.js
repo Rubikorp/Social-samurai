fffd4c58562c7093521d7566b927614e3eccb487
Ruslan Bikmetov <bik.big102rus@gmail.com>
2022-05-02 12:44:06 +0500

    Lesson 51 users api: 1.установил библиотеку axios 2.в Users.jsx сделал get запрос на сервер и полученные данные отправил в setUsers 3.Поменял принимаемые props так как данные с сервера приходят не те что мы планировали 4. изменил стили

--- src/Redux/users-reducer.js
+++ src/Redux/users-reducer.js
@@ -3,64 +3,7 @@ const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET-USERS';
 
 let initialState = {
-  users: [
-    {
-      id:1,
-      photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9_%D0%9C%D0%B5%D0%B4%D0%B2%D0%B5%D0%B4%D0%B5%D0%B2_%2808-04-2022%29.jpg/220px-%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9_%D0%9C%D0%B5%D0%B4%D0%B2%D0%B5%D0%B4%D0%B5%D0%B2_%2808-04-2022%29.jpg',
-      followed: false,
-      fullName:'Ruslan',
-      status: 'I am a boss',
-      location: {
-        city: 'Sterlitamak',
-        country: 'Russian Federation'
-      }
-    },
-    {
-      id:2,
-      photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9_%D0%9C%D0%B5%D0%B4%D0%B2%D0%B5%D0%B4%D0%B5%D0%B2_%2808-04-2022%29.jpg/220px-%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9_%D0%9C%D0%B5%D0%B4%D0%B2%D0%B5%D0%B4%D0%B5%D0%B2_%2808-04-2022%29.jpg',
-      followed: false,
-      fullName:'Elina',
-      status: 'I am a boss',
-      location: {
-        city: 'Sterlitamak',
-        country: 'Russian Federation'
-      }
-    },
-    {
-      id:3,
-      photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9_%D0%9C%D0%B5%D0%B4%D0%B2%D0%B5%D0%B4%D0%B5%D0%B2_%2808-04-2022%29.jpg/220px-%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9_%D0%9C%D0%B5%D0%B4%D0%B2%D0%B5%D0%B4%D0%B5%D0%B2_%2808-04-2022%29.jpg',
-      followed: true,
-      fullName:'Victor',
-      status: 'I am a boss',
-      location: {
-        city: 'Moscow',
-        country: 'Russian Federation'
-      }
-    },
-    {
-      id:4,
-      photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9_%D0%9C%D0%B5%D0%B4%D0%B2%D0%B5%D0%B4%D0%B5%D0%B2_%2808-04-2022%29.jpg/220px-%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9_%D0%9C%D0%B5%D0%B4%D0%B2%D0%B5%D0%B4%D0%B5%D0%B2_%2808-04-2022%29.jpg',
-      followed: true,
-      fullName:'Alexsey',
-      status: 'I am a boss',
-      location: {
-        city: 'Kiev',
-        country: 'Ukraina'
-      }
-    },
-    {
-      id:5,
-      photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9_%D0%9C%D0%B5%D0%B4%D0%B2%D0%B5%D0%B4%D0%B5%D0%B2_%2808-04-2022%29.jpg/220px-%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9_%D0%9C%D0%B5%D0%B4%D0%B2%D0%B5%D0%B4%D0%B5%D0%B2_%2808-04-2022%29.jpg',
-      followed: false,
-      fullName:'Salo',
-      status: 'I am a boss',
-      location: {
-        city: 'Minsk',
-        country: 'Belarus'
-      }
-    },
-
-  ],
+  users: [],
 }
 
 const usersReducer = (state=initialState, action) => {
@@ -68,39 +11,34 @@ const usersReducer = (state=initialState, action) => {
     case FOLLOW:
       return ({
         ...state,
-        //users:[...state.users]
         users: state.users.map(u => {
           if (u.id === action.userID) {
-            return {...u, followed: true}
+            return ({...u, followed: true})
+            return u
           }
-          return u;
         })
       })
-
     case UNFOLLOW:
       return ({
         ...state,
-        //users:[...state.users]
         users: state.users.map(u => {
           if (u.id === action.userID) {
-            return {...u, followed: false}
+            return ({...u, followed: false})
+            return u
           }
-          return u;
         })
       })
-
-    case SET_USERS: {
-      return {...state, users: [...state.users, ...action.users]}
-    }
+    case SET_USERS:
+      return ({
+        ...state, users: [...state.users, ...action.users]
+      })
     default:
       return state
   }
 }
 
-export const followAC = (userID) => ({type: FOLLOW, userID});
-
-export const unFollowAC = (userID) =>({ type: UNFOLLOW, userID})
-
-export const setUsersAC = (users) =>({ type: SET_USERS, users})
+export const followAC = (userID) => ({type: FOLLOW, userID})
+export const unFollowAC = (userID) => ({type: UNFOLLOW, userID})
+export const setUsersAC = (users) => ({type: SET_USERS, users})
 
 export default usersReducer
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,66 +1,75 @@
 import React from "react";
 import styles from "./Users.module.css";
+import * as axios from "axios";
+import userPhoto from "../../assets/img/user.jpg"
 
 const Users = (props) => {
     if (props.users.length === 0) {
-        props.setUsers([
-            {
-                id:1,
-                photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9_%D0%9C%D0%B5%D0%B4%D0%B2%D0%B5%D0%B4%D0%B5%D0%B2_%2808-04-2022%29.jpg/220px-%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9_%D0%9C%D0%B5%D0%B4%D0%B2%D0%B5%D0%B4%D0%B5%D0%B2_%2808-04-2022%29.jpg',
-                followed: false,
-                fullName:'Ruslan',
-                status: 'I am a boss',
-                location: {
-                    city: 'Sterlitamak',
-                    country: 'Russian Federation'
-                }
-            },
-            {
-                id:2,
-                photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9_%D0%9C%D0%B5%D0%B4%D0%B2%D0%B5%D0%B4%D0%B5%D0%B2_%2808-04-2022%29.jpg/220px-%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9_%D0%9C%D0%B5%D0%B4%D0%B2%D0%B5%D0%B4%D0%B5%D0%B2_%2808-04-2022%29.jpg',
-                followed: false,
-                fullName:'Elina',
-                status: 'I am a boss',
-                location: {
-                    city: 'Sterlitamak',
-                    country: 'Russian Federation'
-                }
-            },
-            {
-                id:3,
-                photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9_%D0%9C%D0%B5%D0%B4%D0%B2%D0%B5%D0%B4%D0%B5%D0%B2_%2808-04-2022%29.jpg/220px-%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9_%D0%9C%D0%B5%D0%B4%D0%B2%D0%B5%D0%B4%D0%B5%D0%B2_%2808-04-2022%29.jpg',
-                followed: true,
-                fullName:'Victor',
-                status: 'I am a boss',
-                location: {
-                    city: 'Moscow',
-                    country: 'Russian Federation'
-                }
-            },
-            {
-                id:4,
-                photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9_%D0%9C%D0%B5%D0%B4%D0%B2%D0%B5%D0%B4%D0%B5%D0%B2_%2808-04-2022%29.jpg/220px-%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9_%D0%9C%D0%B5%D0%B4%D0%B2%D0%B5%D0%B4%D0%B5%D0%B2_%2808-04-2022%29.jpg',
-                followed: true,
-                fullName:'Alexsey',
-                status: 'I am a boss',
-                location: {
-                    city: 'Kiev',
-                    country: 'Ukraina'
-                }
-            },
-            {
-                id:5,
-                photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9_%D0%9C%D0%B5%D0%B4%D0%B2%D0%B5%D0%B4%D0%B5%D0%B2_%2808-04-2022%29.jpg/220px-%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9_%D0%9C%D0%B5%D0%B4%D0%B2%D0%B5%D0%B4%D0%B5%D0%B2_%2808-04-2022%29.jpg',
-                followed: false,
-                fullName:'Salo',
-                status: 'I am a boss',
-                location: {
-                    city: 'Minsk',
-                    country: 'Belarus'
-                }
-            },
 
-        ])
+        axios
+          .get("https://social-network.samuraijs.com/api/1.0/users")
+          .then(response => {
+              props.setUsers(response.data.items)
+          })
+
+        // props.setUsers([
+        //     {
+        //         id:1,
+        //         photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9_%D0%9C%D0%B5%D0%B4%D0%B2%D0%B5%D0%B4%D0%B5%D0%B2_%2808-04-2022%29.jpg/220px-%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9_%D0%9C%D0%B5%D0%B4%D0%B2%D0%B5%D0%B4%D0%B5%D0%B2_%2808-04-2022%29.jpg',
+        //         followed: false,
+        //         fullName:'Ruslan',
+        //         status: 'I am a boss',
+        //         location: {
+        //             city: 'Sterlitamak',
+        //             country: 'Russian Federation'
+        //         }
+        //     },
+        //     {
+        //         id:2,
+        //         photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9_%D0%9C%D0%B5%D0%B4%D0%B2%D0%B5%D0%B4%D0%B5%D0%B2_%2808-04-2022%29.jpg/220px-%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9_%D0%9C%D0%B5%D0%B4%D0%B2%D0%B5%D0%B4%D0%B5%D0%B2_%2808-04-2022%29.jpg',
+        //         followed: false,
+        //         fullName:'Elina',
+        //         status: 'I am a boss',
+        //         location: {
+        //             city: 'Sterlitamak',
+        //             country: 'Russian Federation'
+        //         }
+        //     },
+        //     {
+        //         id:3,
+        //         photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9_%D0%9C%D0%B5%D0%B4%D0%B2%D0%B5%D0%B4%D0%B5%D0%B2_%2808-04-2022%29.jpg/220px-%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9_%D0%9C%D0%B5%D0%B4%D0%B2%D0%B5%D0%B4%D0%B5%D0%B2_%2808-04-2022%29.jpg',
+        //         followed: true,
+        //         fullName:'Victor',
+        //         status: 'I am a boss',
+        //         location: {
+        //             city: 'Moscow',
+        //             country: 'Russian Federation'
+        //         }
+        //     },
+        //     {
+        //         id:4,
+        //         photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9_%D0%9C%D0%B5%D0%B4%D0%B2%D0%B5%D0%B4%D0%B5%D0%B2_%2808-04-2022%29.jpg/220px-%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9_%D0%9C%D0%B5%D0%B4%D0%B2%D0%B5%D0%B4%D0%B5%D0%B2_%2808-04-2022%29.jpg',
+        //         followed: true,
+        //         fullName:'Alexsey',
+        //         status: 'I am a boss',
+        //         location: {
+        //             city: 'Kiev',
+        //             country: 'Ukraina'
+        //         }
+        //     },
+        //     {
+        //         id:5,
+        //         photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9_%D0%9C%D0%B5%D0%B4%D0%B2%D0%B5%D0%B4%D0%B5%D0%B2_%2808-04-2022%29.jpg/220px-%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9_%D0%9C%D0%B5%D0%B4%D0%B2%D0%B5%D0%B4%D0%B5%D0%B2_%2808-04-2022%29.jpg',
+        //         followed: false,
+        //         fullName:'Salo',
+        //         status: 'I am a boss',
+        //         location: {
+        //             city: 'Minsk',
+        //             country: 'Belarus'
+        //         }
+        //     },
+        //
+        // ])
     }
 
     return (
@@ -69,7 +78,11 @@ const Users = (props) => {
                 props.users.map(u => <div className={styles.user__item} key={u.id}>
                     <div className={styles.users__left}>
                         <div>
-                            <img className={styles.users__avatar} src={u.photoUrl} alt=""/>
+                            <img
+                              className={styles.users__avatar}
+                              src={u.photos.small != null
+                                ? u.photos.small
+                              : userPhoto} alt=""/>
                         </div>
                         <div>
                             <button
@@ -86,7 +99,7 @@ const Users = (props) => {
                     <div className={styles.users__right}>
                         <div className={styles.user__info}>
                             <div className={styles.username}>
-                                {u.fullName}
+                                {u.name}
                             </div>
                             <div className={styles.userstatus}>
                                 {u.status}
@@ -94,9 +107,9 @@ const Users = (props) => {
                         </div>
                         <div className={styles.user__location}>
                             Country:
-                            <div>{u.location.country}</div>
+                            <div>{"u.location.country"}</div>
                             City:
-                            <div>{u.location.city}</div>
+                            <div>{"u.location.city"}</div>
                         </div>
                     </div>
                 </div>)
