c4c4a25fba9ae24984a839388bfe3b02ab5434a9
Ruslan Bikmetov <bik.big102rus@gmail.com>
2022-05-03 13:29:27 +0500

    Lesson 54: Жизненный цикл 1.В классовой компоненте UsersC.jsx нашу загрузку с сервера пользователей засунул в метод componentDidMount т.е. в момент монтирования компоненты

--- src/components/Users/UsersC.jsx
+++ src/components/Users/UsersC.jsx
@@ -6,6 +6,9 @@ import userPhoto from "../../assets/img/user.jpg"
 class Users extends React.Component {
   constructor(props) {
     super(props);
+  }
+
+  componentDidMount() {
     if (this.props.users.length === 0 ) {
       axios
         .get("https://social-network.samuraijs.com/api/1.0/users")
