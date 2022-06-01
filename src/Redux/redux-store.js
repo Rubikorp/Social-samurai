96702be60231ac8cc49f58fb46f69d0ad7205824
Ruslan Bikmetov <bik.big102rus@gmail.com>
2022-06-01 15:26:19 +0500

    Подключили extension для расширения redux devtools

--- src/Redux/redux-store.js
+++ src/Redux/redux-store.js
@@ -1,4 +1,4 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, compose, createStore} from "redux";
 import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dialogs-reducer";
 import usersReducer from "./users-reducer";
@@ -15,7 +15,13 @@ let reducers = combineReducers({
     app: appReducer
 })
 
-let store = createStore(reducers, applyMiddleware(thunkMiddleware))
+//добавляем расширение redux-devtools
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, /* preloadedState, */ composeEnhancers(
+    applyMiddleware(thunkMiddleware)
+));
+
+// let store = createStore(reducers, applyMiddleware(thunkMiddleware))
 
 export default store
 window.store = store
